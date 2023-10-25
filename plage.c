87f2e5f5d527ae9f3ce6606029e446a0f0c25f9a
RVieira <vieira.ruben.sp@gmail.com>
2023-10-25 14:10:06 +0200

    modifs + modele

--- plage.c
+++ plage.c
@@ -121,6 +121,23 @@ int ppm_read(SURFACE *s, FILE *f) {
     return 1;
 }
 
+void linear_gradient(struct Pixel startColor, struct Pixel endColor, int x, int y, SURFACE *s) {
+    float stepR = (endColor.red - startColor.red) / (float)width;
+    float stepG = (endColor.green - startColor.green) / (float)width;
+    float stepB = (endColor.blue - startColor.blue) / (float)width;
+
+    for (int i = y; i < y + height; ++i) {
+        struct Pixel currentColor;
+        currentColor.red = (unsigned char)(startColor.red + stepR * (i - x));
+        currentColor.green = (unsigned char)(startColor.green + stepG * (i - x));
+        currentColor.blue = (unsigned char)(startColor.blue + stepB * (i - x));
+
+        for (int j = x; j < x + width; ++j) {
+            s->data[j * s->width + i] = currentColor;
+        }
+    }
+}
+
 void draw_rectangle(SURFACE *s, int x1, int y1, int x2, int y2, struct Pixel color) {
     if (x1 < 0 || x1 > s->width || y1 < 0 || y1 > s->height ||
         x2 < 0 || x2 > s->width || y2 < 0 || y2 > s->height) {
@@ -229,32 +246,53 @@ void courbe_bezier(SURFACE *s,struct Point P1,struct Point P2,struct Point P3,st
     }
 }
 
-void _fill(SURFACE *s, struct Pixel targetColor,struct Pixel fillColor, int startY, int endY) {
-    for (int y = startY; y < endY; y++) {
-        for (int x = 0; x < s->width; x++) {
-            struct Pixel *currentPixel = &s->data[y * s->width + x];
-            if (currentPixel->red == targetColor.red && currentPixel->green == targetColor.green && currentPixel->blue == targetColor.blue) {
-                *currentPixel = fillColor;
-            } else {
-                break; 
+void courbe_bezier_epaisse(SURFACE *s, struct Point P1, struct Point P2, struct Point P3, struct Point P4, int N, struct Pixel couleur, int largeur) {
+    for (double t = 0; t < 1; t += 1.0 / N) {
+        double x = P1.x * pow((1 - t), 3) + 3 * P2.x * t * pow((1 - t), 2) + 3 * P3.x * pow(t, 2) * (1 - t) + P4.x * pow(t, 3);
+        double y = P1.y * pow((1 - t), 3) + 3 * P2.y * t * pow((1 - t), 2) + 3 * P3.y * pow(t, 2) * (1 - t) + P4.y * pow(t, 3);
+        struct Point p;
+        p.x = x;
+        p.y = y;
+        
+        for (int i = -largeur; i <= largeur; i++) {
+            for (int j = -largeur; j <= largeur; j++) {
+                struct Point offset;
+                offset.x = p.x + i;
+                offset.y = p.y + j;
+                draw_point(s, offset, couleur);
             }
         }
     }
 }
 
-void apply_perlin_noise(SURFACE *s, struct Pixel targetColor, int startY, int endY,float freq,int depth) {
-    for (int y = startY; y < endY; y++) {
-        for (int x = 0; x < s->width; x++) {
+
+void fill_vert(SURFACE *s, struct Pixel targetColor, struct Pixel fillColor, int startX, int endX,int startY, int endY) {
+    for (int x = startX; x < endX; x++) {
+        for (int y = startY; y < endY; y++) {
             struct Pixel *currentPixel = &s->data[y * s->width + x];
-            if (currentPixel->red == targetColor.red && currentPixel->green == targetColor.green && currentPixel->blue == targetColor.blue) {
-                float noise = perlin2d(x, y, freq, depth);
-                currentPixel->red = ((unsigned int)((1+noise)*127.5) + currentPixel->red)/2;
-                currentPixel->green = ((unsigned int)((1+noise)*127.5) + currentPixel->green)/2;
-                currentPixel->blue = ((unsigned int)((1+noise)*127.5) + currentPixel->blue)/2;
+            if(currentPixel->red == targetColor.red && currentPixel->green == targetColor.green && currentPixel->blue == targetColor.blue){
+                break;
             }
+            *currentPixel = fillColor;
         }
     }
 }
+
+
+
+// void apply_perlin_noise(SURFACE *s, struct Pixel targetColor, int startY, int endY,float freq,int depth) {
+//     for (int y = startY; y < endY; y++) {
+//         for (int x = 0; x < s->width; x++) {
+//             struct Pixel *currentPixel = &s->data[y * s->width + x];
+//             if (currentPixel->red == targetColor.red && currentPixel->green == targetColor.green && currentPixel->blue == targetColor.blue) {
+//                 float noise = perlin2d(x, y, freq, depth);
+//                 currentPixel->red = ((unsigned int)((1+noise)*127.5) + currentPixel->red)/2;
+//                 currentPixel->green = ((unsigned int)((1+noise)*127.5) + currentPixel->green)/2;
+//                 currentPixel->blue = ((unsigned int)((1+noise)*127.5) + currentPixel->blue)/2;
+//             }
+//         }
+//     }
+// }
 
     
 
@@ -263,24 +301,24 @@ int main(){
     surface(&surf,1000,1000);
     assert(surf.data != NULL);
 
-    struct Pixel sky = {173,252,252};
-    struct Pixel sand = {236,209,161};
+    struct Pixel sky = {99,201,250};
+    struct Pixel sky2 = {223,247,251};
+    struct Pixel sand = {241,170,80};
     struct Pixel foam = {255,255,255};
-    struct Pixel water = {106,226,204};
-    struct Point P1 = {167,1000};
-    struct Point P2 = {580,871};
-    struct Point P3 = {270,795};
-    struct Point P4 = {437,734};
-    struct Point P5 = {534,667};
-    struct Point P6 = {394,559};
-    struct Point P7 = {473,500};
-    
+    struct Pixel water1 = {9,153,226};
+    struct Pixel water2 = {25,192,234};
+    struct Point P1 = {0,701}; struct Point P2 = {196,728}; struct Point P3 = {280,802}; struct Point P4 = {385,726}; struct Point P5 = {527,678}; struct Point P6 = {583,768}; struct Point P7 = {735,654}; struct Point P8 = {787,614}; struct Point P9 = {930,724}; struct Point P10 = {1000,591};
+    struct Point P11 = {0,696}; struct Point P12 = {174,664}; struct Point P13 = {282,718}; struct Point P14 = {385,721};
+
     fill(&surf,sky);
-    draw_rectangle(&surf,0,500,1000,1000,sand);
-    courbe_bezier(&surf,P1,P2,P3,P4,2000,foam);
-    courbe_bezier(&surf,P4,P5,P6,P7,2000,foam);
-    _fill(&surf,sand,water,500,1000);
-    apply_perlin_noise(&surf,water,500,1000,0.2,3);
+    linear_gradient(sky,sky2,surf.width,surf.height,0,0,&surf);
+    draw_rectangle(&surf,0,450,1000,1000,sand);
+    courbe_bezier_epaisse(&surf,P1,P2,P3,P4,5000,foam,10);
+    courbe_bezier_epaisse(&surf,P4,P5,P6,P7,5000,foam,10);
+    courbe_bezier_epaisse(&surf,P7,P8,P9,P10,5000,foam,10);
+    // courbe_bezier_epaisse(&surf,P11,P12,P13,P14,5000,water2,10);
+    fill_vert(&surf,foam,water1,0,1000,450,1000);
+   
 
     FILE *output = fopen("draw.ppm","w");
     assert(output != NULL);
